4d6c03334f1030282f7b5e0673d2864de2a6c48a
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-05-28 20:12:44 -0500

    add navigation bar inspired by Next.js tutorial

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
+// [layout.jsx]
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SideNav from "./ui/sideNav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <SideNav />
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }

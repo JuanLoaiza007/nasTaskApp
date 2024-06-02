0fd25fd098462471169367452887709bcbdf4113
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-06-02 02:11:55 -0500

    updated connection

--- src/app/libs/postgres.js
+++ src/app/libs/postgres.js
@@ -1,9 +1,3 @@
 import postgres from "postgres";
 
-export const conn = postgres({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-});
+export const conn = postgres(process.env.POSTGRES_URL);

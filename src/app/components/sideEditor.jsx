7f4887f0d69762fea0186a9069a9b94c116da7df
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-06-01 19:34:38 -0500

    visibility improvements

--- src/app/components/sideEditor.jsx
+++ src/app/components/sideEditor.jsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import { SideEditorState } from "@/states/SideEditorState";
 
 export default function SideEditor() {
@@ -8,15 +7,15 @@ export default function SideEditor() {
   return (
     <>
       {useSideEditorState.isEditing && (
-        <div className="max-w-lg mx-2 px-2">
+        <div className="w-full md:max-w-xs">
           <button
             className="
-            px-4 py-2 my-4 text-white bg-blue-600 rounded-lg hover:bg-blue-400 hover:text-white hover:cursor-pointer"
+            px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-400 hover:text-white hover:cursor-pointer"
             onClick={() => {
               useSideEditorState.setIsEditing(false);
             }}
           >
-            {">"}
+            {"X"}
           </button>
           {useSideEditorState.component}
         </div>
